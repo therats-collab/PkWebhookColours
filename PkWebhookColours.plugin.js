41d009d6e4a56b84388aeadccdb509823138bd66
The Rats <dev@rats.world>
2020-08-19 01:16:47 +0930

    Updated to using versioned endpoint
    They've been deprecated for like...a year, and officially Don't Work Any More, so I fixed it. Also removed some debug console logging stuff, and incremented plugin to version 0.2.

--- PkWebhookColours.plugin.js
+++ PkWebhookColours.plugin.js
@@ -7,7 +7,7 @@
 class PluralKitWebhookColours {
   getName() {return "PluralKit Webhook Colours";}
   getDescription() {return "Modifies PluralKit webhook colours to match the colour a member has set in their profile.\n(Member must not be private!)";}
-  getVersion() {return "0.1";}
+  getVersion() {return "0.2";}
   getAuthor() {return "lgs";}
 
   load() {}
@@ -17,15 +17,13 @@ class PluralKitWebhookColours {
   observer(changes) {
     var node;
     var message;
-    console.log("Hi!")
     try {
       var nodeList = changes.addedNodes;
       for (let i = 0; i < nodeList.length; i++) {
         let item = nodeList[i];
-        console.log("Hi!2")
         message = item.__reactInternalInstance$.memoizedProps.children[1].props.message;
         if (message.author.bot) {
-          var PkRequest = new Request('https://api.pluralkit.me/msg/' + message.id);
+          var PkRequest = new Request('https://api.pluralkit.me/v1/msg/' + message.id);
           fetch(PkRequest)
           .then(response => { if (response.status === 200) { return response.text(); } else { } })
           .then(response => { try { item.children[0].children[1].children[0].style.color = `#${JSON.parse(response).member.color}`; } catch (e) {}; });
